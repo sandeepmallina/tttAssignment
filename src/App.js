8d1c3bbb777a2ae915ef8338bbf006cddd838123
sandeepmallina <73931070+sandeepmallina@users.noreply.github.com>
2023-05-15 04:45:21 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Showbutton } from "./Showbutton";
+import "./styles.css";
+import TextMining from "./TextMining";
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* <Showbutton /> */}
+      <TextMining />
+    </div>
+  );
+}
